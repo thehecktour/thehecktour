e8479602cefeb2baa3c755f6006b1de4d7d28154
Atilio <atilio.jesus@grupoboticario.com.br>
2024-10-20 22:54:44 -0300

    chore: 🚧 remove biblioteca httpx

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
+@app.get("/api/v1/users")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
