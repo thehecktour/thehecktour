9f5490c2642e6ef42bc30a7ae56397bd88d87a50
Atilio <atilio.jesus@grupoboticario.com.br>
2024-10-20 23:04:08 -0300

    feat: ✨ inputa metodos por dentro do .env

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from modules.http.data_fetcher import DataFetcher
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
-csv_url = "https://storage.googleapis.com/juntossomosmais-code-challenge/input-backend.csv"
-json_url = "https://storage.googleapis.com/juntossomosmais-code-challenge/input-backend.json"
+csv_url = os.getenv("CSV_URL")
+json_url = os.getenv("JSON_URL")
 
 data_fetcher = DataFetcher(csv_url, json_url)
 data_fetcher.fetch_csv()
@@ -13,13 +17,13 @@ data_fetcher.fetch_json()
 @app.get("/api/v1/users/csv")
 def get_csv_data():
     if data_fetcher.data_csv is not None:
-        return data_fetcher.data_csv.to_dict(orient="records")  
+        return data_fetcher.data_csv.to_dict(orient="records")
     else:
         return {"error": "CSV data not found"}, 404
 
 @app.get("/api/v1/users/json")
 def get_json_data():
     if data_fetcher.data_json is not None:
-        return data_fetcher.data_json  
+        return data_fetcher.data_json
     else:
         return {"error": "JSON data not found"}, 404
